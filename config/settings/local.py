04bb151d8cabc553d180ff0e2065710c3c15b18a
Taylor <taylor@syxo.tennis>
2025-12-19 15:02:53 +0700

    Init && add base settings

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t6@r@i3y2r0hrey!6nn(1i023nf7prteq!vn)c%3&u+me23dd%"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
