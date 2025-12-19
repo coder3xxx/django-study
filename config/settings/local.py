cc0ea4d5bac4fc364b01e7e842e7f29751e9cea6
Taylor <taylor@syxo.tennis>
2025-12-19 15:15:33 +0700

    Local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,27 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t6@r@i3y2r0hrey!6nn(1i023nf7prteq!vn)c%3&u+me23dd%"
+from django.conf.global_settings import EMAIL_BACKEND
+from .base import *  # noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+SITE_NAME = getenv("SITE_NAME")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
